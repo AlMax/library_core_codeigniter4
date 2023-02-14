51c434c6c9da6bbc271c2d9d650a35fcf4b43357
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2023-02-14 10:32:51 +0100

    CI model

--- src/Core/CoreModel.php
+++ src/Core/CoreModel.php
@@ -6,19 +6,5 @@ use CodeIgniter\Model;
 
 abstract class CoreModel
 {
-    private string $type;
 
-    public function __construct(
-        $type = 'class'
-    )
-    {
-        $this->type = $type;
-
-        switch ($this->type) {
-            case 'codeigniter':
-                return new Model();
-        }
-
-        return $this;
-    }
 }
---
+++ src/Core/CoreModel_codeigniter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CoreLibrary\Core;
+
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+use CodeIgniter\Validation\ValidationInterface;
+
+abstract class CoreModel_codeigniter extends Model
+{
+    protected $table = '';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [];
+    protected $useTimestamps = true;
+
+    protected $returnType = 'object';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    protected $useSoftDeletes = false;
+    protected $deletedField = 'deleted_at';
+
+    public function __construct(
+        string               $table,
+        array                $allowedFields,
+        string               $softDeletesField = null,
+        ?ConnectionInterface $db = null
+    )
+    {
+        $this->table = $table;
+        $this->allowedFields = $allowedFields;
+        if ($softDeletesField !== null) {
+            self::setSoftDeletes($softDeletesField);
+        }
+
+        parent::__construct($db);
+    }
+
+    private function setSoftDeletes(
+        string $deletedField
+    )
+    {
+        $this->useSoftDeletes = true;
+        $this->deletedField = $deletedField;
+    }
+
+}
